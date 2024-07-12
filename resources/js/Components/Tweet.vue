55e5a666bec609ecde11a0af6cb43c075e60ed37
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2024-07-12 18:24:39 +0300

    Full commit

---
+++ resources/js/Components/Tweet.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
+import ChartBar from 'vue-material-design-icons/ChartBar.vue'
+import MessageOutline from 'vue-material-design-icons/MessageOutline.vue'
+import Sync from 'vue-material-design-icons/Sync.vue'
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue'
+import TrashCanOutline from 'vue-material-design-icons/TrashCanOutline.vue'
+
+defineProps({
+    tweet: {
+        type: Object,
+        required: true
+    }
+});
+
+let openOptions = ref(false);
+
+</script>
+
+<template>
+    <div class="min-w"-[60px]>
+        <img :src="tweet.image" class="rounded-full m-2 mt-3" width="50">
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+import TwitterLayout from "@/Layouts/TwitterLayout.vue";
+</script>
+
+<template>
+    <Head title="Анонимная соцсеть" />
+    <TwitterLayout>
+        <div class="text-white">
+            <Tweet :tweet="{
+                id: 1,
+                text: 'Тестовая запись',
+                user: {
+                    name: 'Аноним',
+                    avatar: 'https://i.imgur.com/0333333.png'
+                },
+                created_at: '2021-09-20 12:00:00',
+                likes: 10,
+                comments: 1
+            }"/>
+            <div class="border-b border-b-gray-800 mt-2"></div>
+        </div>
+    </TwitterLayout>
+</template>
